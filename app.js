f6ff2c409af79d399fbcbb6335ce96e3927bbaff
Nicolas Costa <nicolas@Nicolass-MacBook-Air.local>
2023-06-06 16:39:22 -0300

    agregado de responsive y mejoras del sitio

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
 });
